41fb56b8f6dd4267229aa7ac526f01f275a21428
Amol Nakhate <106758417+amol64546@users.noreply.github.com>
2026-09-22 23:59:27 +0530

    Update Vite base path and GitHub Pages URL to match repo name

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/etl-orchestration-studio/',
+  base: '/etl-orchestration-ui/',
   plugins: [react()],
   server: {
     port: 3000,
     // proxy: {
     //   '/api': {
-    //     target: 'https://seatunnel-orchestrator-1-0-0.onrender.com',
-    //     changeOrigin: true,
-    //     rewrite: (path) => path.replace(/^\/api/, '')
+    //   target: 'https://seatunnel-orchestrator-1-0-0.onrender.com',
+    //   changeOrigin: true,
+    //   rewrite: (path) => path.replace(/^\/api/, '')
     //   }
     // }
   }
